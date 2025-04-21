fc8d4e658cc17f23dce71f9226895f3ad230e81b
paigauresh <paigauresh@gmail.com>
2025-04-21 22:53:16 +0530

    build issues resolved and packages updated

--- lib/getUser.ts
+++ lib/getUser.ts
@@ -5,8 +5,8 @@ import { revalidatePath } from "next/cache";
 export const getUser = async() => {
     dbConnect();
 
-    const session = getServerSession();
-    const userId = session?.user.id;
+    const session = await getServerSession();
+    const userId = session?.user?.id;
     if(!userId){
         revalidatePath("/sign-in")
         throw new Error("User is not authorized")
--- middleware.ts
+++ middleware.ts
@@ -22,7 +22,6 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/", request.url))
     }
   }
-
-
+  
   return NextResponse.next();
 }
