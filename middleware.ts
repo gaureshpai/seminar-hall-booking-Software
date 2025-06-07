9db2d58167123bfcd7549ed0eb10d7ce92c85edd
paigauresh <paigauresh@gmail.com>
2025-06-07 22:53:47 +0530

    allow page view without sign in

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,6 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
-  if (url.pathname === "/") {
-      if (!token) {
-          return NextResponse.redirect(new URL("/sign-up", request.url));
-      }
-      return NextResponse.next();
-  }
   if(url.pathname.startsWith("/sign-up")){
     if(token){
       return NextResponse.redirect(new URL("/sign-in", request.url))
